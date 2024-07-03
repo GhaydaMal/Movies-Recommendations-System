2a29f3501887507606b751a637e2e6408b8fda86
Ghayda Almalki <163356014+GhaydaMal@users.noreply.github.com>
2024-07-03 23:51:18 +0300

    Add files via upload

--- appp.py
+++ appp.py
@@ -1,26 +1,26 @@
 import pickle
-import streamlit as st
-import requests
+import streamlit as st #building interactive web applications with Python UI
+import requests #fetch data from external API
 
-#fetch poster using moive id in moives API
+#fetch poster img using movie_id from movies API
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id) # fetch based on spesific moive id
+    data = requests.get(url) #reterive info about that poster
+    data = data.json() #convert to json
+    poster_path = data['poster_path'] #takes only the img path from json file
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path #add the img path to this path
     return full_path
 
 
-# recommend moives based on title and return title and poster
+# recommend movies similer to the input, return both title and poster
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
     for i in distances[1:6]:
-        # fetch the movie poster
-        movie_id = movies.iloc[i[0]].movie_id
+        # fetch the movie detailes (id, poster , title)
+        movie_id = movies.iloc[i[0]].movie_id #fetch movie id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
 
@@ -40,6 +40,7 @@ selected_movie = st.selectbox(
     movie_list
 )
 
+# Button to trigger recommendations
 if st.button('Show Recommendations'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
     col1, col2, col3, col4, col5 = st.columns(5)
